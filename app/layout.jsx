9c83cc822295fe2045ce02acf46e9f5142b614de
brayan-cincoe <brayanrcaicedo@gmail.com>
2023-09-24 19:21:12 -0500

    style: add default font

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import Navbar from "../components/Navbar";
+import { Roboto } from "next/font/google";
 
 export const metadata = {
   title: 'App tienda - Inicio',
@@ -6,10 +7,16 @@ export const metadata = {
   keywords: 'tienda, online, comercio'
 }
 
+const roboto = Roboto({
+  weight: ["300", "400", "500", "700"],
+  style: ["italic", "normal"],
+  subsets: ['latin']
+})
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className={roboto.className} >
         <Navbar />
         {children}
       </body>
