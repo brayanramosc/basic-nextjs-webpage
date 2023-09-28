f174bd6e0906e4e6b2f65cc044cb312e4bc1cda6
brayan-cincoe <brayanrcaicedo@gmail.com>
2023-09-27 21:10:21 -0500

    feat: fetching data from server side

---
+++ app/posts/page.jsx
@@ -0,0 +1,24 @@
+import PostCard from "@/components/PostCard";
+
+const loadPosts = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const data = await res.json();
+
+    return data;
+}
+
+const PostPage = async () => {
+    const posts = await loadPosts();
+
+    return (
+        <div>
+            {
+                posts.map(post =>
+                    <PostCard post={post} key={post.id} />
+                )
+            }
+        </div>
+    )
+}
+
+export default PostPage;
